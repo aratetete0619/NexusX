1cc5de11f73b1611b9f1eb001ef64444d03c576b
teteteo619 <120061560+aratetete0619@users.noreply.github.com>
2023-08-09 17:57:57 +0900

    Update demo data creation script

--- backend/demo_data_creater.py
+++ backend/demo_data_creater.py
@@ -4,6 +4,7 @@ from elasticsearch import Elasticsearch
 from neo4j import GraphDatabase
 from faker import Faker
 from pathlib import Path
+import random
 
 fake = Faker()
 
@@ -15,20 +16,39 @@ password = os.getenv("NEO4J_PASSWORD")
 driver = GraphDatabase.driver("neo4j://localhost:7687", auth=("neo4j", password))
 
 
-def add_person(tx, name, es_id, image_path):
-    tx.run(
-        "CREATE (:Person {name: $name, es_id: $es_id, image_path: $image_path})",
+def add_person_with_skills(tx, name, person_es_id, image_path, skills):
+    # パーソンノードを作成
+    result = tx.run(
+        "CREATE (p:Person {name: $name, es_id: $person_es_id, image_path: $image_path}) RETURN p",
         name=name,
-        es_id=es_id,
+        person_es_id=person_es_id,
         image_path=image_path,
     )
+    person_node = result.single()[0]
+
+    for skill_name, skill_es_id in skills:
+        # スキルノードを作成または取得
+        result = tx.run(
+            "MERGE (s:Skill {name: $skill_name, es_id: $skill_es_id}) RETURN s",
+            skill_name=skill_name,
+            skill_es_id=skill_es_id,
+        )
+        skill_node = result.single()[0]
+
+        # パーソンノードとスキルノードを関連付け
+        tx.run(
+            "MATCH (p:Person), (s:Skill) WHERE id(p) = $person_id AND id(s) = $skill_id "
+            "CREATE (p)-[:HAS_SKILL]->(s)",
+            person_id=person_node.id,
+            skill_id=skill_node.id,
+        )
 
 
 def download_image(image_url, save_dir):
     response = requests.get(image_url, stream=True)
     response.raise_for_status()
 
-    # 画像ファイル名を生成（ここではランダムな文字列を使用しますが、適切なファイル名を使用してください）
+    # 画像ファイル名を生成
     image_file_name = f"{fake.unique.random_number(digits=6)}.jpg"
     image_path = Path(save_dir) / image_file_name
 
@@ -44,16 +64,33 @@ for _ in range(1000):  # 生成するデータの数を設定
     name = fake.name()
     description = fake.text()
 
-    # Elasticsearchに保存
-    es_res = es.index(index="people", body={"description": description})
-    es_id = es_res["_id"]
-
     # ランダムな画像をダウンロード
     image_url = "https://picsum.photos/200"  # 200x200のランダムな画像を生成
     image_path = download_image(image_url, "./demo_images")
 
+    # Elasticsearchに人物の詳細を保存
+    person_es_res = es.index(
+        index="people",
+        body={"name": name, "description": description, "image_path": image_path},
+    )
+    person_es_id = person_es_res["_id"]
+
+    # ランダムなスキルを生成
+    skills = []
+    for _ in range(random.randint(1, 5)):
+        skill_name = fake.job()
+        skill_description = fake.text()
+        # Elasticsearchにスキルの詳細を保存
+        skill_es_res = es.index(
+            index="skills", body={"name": skill_name, "description": skill_description}
+        )
+        skill_es_id = skill_es_res["_id"]
+        skills.append((skill_name, skill_es_id))
+
     # Neo4jに保存
     with driver.session() as session:
-        session.write_transaction(add_person, name, es_id, image_path)
+        session.write_transaction(
+            add_person_with_skills, name, person_es_id, image_path, skills
+        )
 
 driver.close()
