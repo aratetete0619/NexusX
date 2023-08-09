d2235c5d0fb465855606d65b4b265e2371044409
teteteo619 <120061560+aratetete0619@users.noreply.github.com>
2023-08-09 17:57:44 +0900

    Refactor authentication utilities

--- backend/app/utils/auth.py
+++ backend/app/utils/auth.py
@@ -1,15 +1,25 @@
 # app/utils/auth.py
 
-import bcrypt
 import re
 from fastapi import HTTPException
 from passlib.context import CryptContext
+import jwt
+from datetime import datetime, timedelta
+import uuid
+import logging
+import redis
+
+logging.basicConfig(level=logging.INFO)
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hash_password(password: str):
     return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
 
 
 def check_password_strength(password: str):
@@ -30,3 +40,41 @@ def check_password_strength(password: str):
         or symbol_error
     )
     return password_ok
+
+
+def generate_auth_token(user_id, secret_key):
+    logging.info("generate_auth_token called")
+    payload = {
+        "user_id": user_id,
+        "iat": datetime.utcnow(),
+        "exp": datetime.utcnow() + timedelta(days=1),
+    }
+    return jwt.encode(payload, secret_key, algorithm="HS256")
+
+
+def verify_auth_token(token, secret_key):
+    try:
+        payload = jwt.decode(token, secret_key, algorithms=["HS256"])
+        return payload["user_id"]
+    except jwt.PyJWTError:
+        return None
+
+
+def generate_confirmation_code():
+    confirmation_code = uuid.uuid4().hex
+    return confirmation_code
+
+
+r = redis.Redis(host="localhost", port=6379, db=0)
+
+
+def track_account_creation(ip_address):
+    current_time = datetime.now().timestamp()
+    r.lpush(ip_address, current_time)
+    # Only keep account creations in the last hour
+    one_hour_ago = (datetime.now() - timedelta(hours=1)).timestamp()
+    r.ltrim(ip_address, 0, int(one_hour_ago))
+    if len(r.lrange(ip_address, 0, -1)) > 5:
+        raise Exception(
+            "Too many account creations from this IP address. Please try again later."
+        )
