6eedf52efc26ce1dff601de53e8b0ac0cd82ebe6
teteteo619 <120061560+aratetete0619@users.noreply.github.com>
2023-08-09 17:57:53 +0900

    Modify main application entry point

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,38 @@
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import PlainTextResponse, JSONResponse
+from fastapi.responses import PlainTextResponse, JSONResponse, RedirectResponse
 from starlette.middleware.cors import CORSMiddleware
 from starlette_graphene3 import GraphQLApp
 from .graphql.schema import schema
+from logging_config import LOGGING_CONFIG
+from starlette.middleware.base import BaseHTTPMiddleware
+import jwt
+import os
+from dotenv import load_dotenv
+from logging_config import LOGGING_CONFIG
+from .graphql.resolvers import resolve_confirm_user
+from apscheduler.schedulers.background import BackgroundScheduler
+from .utils.database import create_connection, close_connection
+
+
+load_dotenv()
+
+
+class AuthMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        auth_header = request.headers.get("Authorization")
+
+        if auth_header:
+            try:
+                token = auth_header.split(" ")[1]
+                jwt.decode(token, os.getenv("JWT_SECRET"), algorithms=["HS256"])
+            except (IndexError, jwt.DecodeError, jwt.ExpiredSignatureError):
+                return JSONResponse(
+                    status_code=401,
+                    content={"message": "Invalid or expired token"},
+                )
+
+        response = await call_next(request)
+        return response
 
 
 class UserAlreadyExistsError(Exception):
@@ -17,6 +47,22 @@ class InvalidPasswordError(Exception):
         super().__init__(self.message)
 
 
+def delete_unconfirmed_users():
+    # Create a new connection for the job
+    conn = create_connection()
+
+    # Your job code here
+
+    # Close the connection when the job is done
+    close_connection(conn)
+
+
+def start_scheduler():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(delete_unconfirmed_users, "interval", hours=24)
+    scheduler.start()
+
+
 app = FastAPI()
 
 app.add_middleware(
@@ -26,6 +72,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.add_middleware(AuthMiddleware)
 
 app.add_route("/graphql", GraphQLApp(schema=schema))
 
@@ -51,7 +99,16 @@ async def handle_invalid_password(error: InvalidPasswordError):
     )
 
 
-if __name__ == "__main__":
-    import uvicorn
+@app.get("/confirm/{confirmation_code}")
+async def confirm(confirmation_code: str):
+    result = resolve_confirm_user(confirmation_code)
+    if result["success"]:
+        # if the confirmation was successful, redirect to the login page
+        return RedirectResponse(url="/login", status_code=303)
+    else:
+        # else, return a plain text response with the error message
+        return PlainTextResponse(result["message"], status_code=400)
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_config=LOGGING_CONFIG)
