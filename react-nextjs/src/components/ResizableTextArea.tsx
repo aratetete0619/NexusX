833d041932fd0bf83ac8cb863a0ac983e57b7109
teteteo619 <120061560+aratetete0619@users.noreply.github.com>
2023-08-16 00:46:30 +0900

    :add: Add error handling and loading for searcbar

--- react-nextjs/src/components/ResizableTextArea.tsx
+++ react-nextjs/src/components/ResizableTextArea.tsx
@@ -1,14 +1,14 @@
+// components/ResizableTextArea.tsx
 import { setFocus, setResults, updateQuery, selectPolarNode } from '../redux/actions';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useContext } from 'react';
 import { useDispatch } from 'react-redux';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { useQuery } from '@apollo/client';
+import { useQuery, ApolloError } from '@apollo/client';
 import styles from '../styles/SearchArea.module.css';
 import { RESIZABLE_SEARCH_QUERY } from '../graphql/mutations'
 import Loader from './Loader';
 import { ErrorContext } from '../contexts/ErrorContext';
-
 
 
 interface ResizableTextAreaProps {
@@ -21,7 +21,12 @@ interface ResizableTextAreaProps {
 const ResizableTextArea: React.FC<ResizableTextAreaProps> = ({ setIsCircle, query, setQuery, setShowEdges }) => {
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
   const dispatch = useDispatch();
-  const { data, refetch } = useQuery(RESIZABLE_SEARCH_QUERY, { skip: true });
+  const { data, refetch, loading, error } = useQuery(RESIZABLE_SEARCH_QUERY, { skip: true });
+  const errorContext = useContext(ErrorContext);
+  if (!errorContext) {
+    throw new Error('ErrorContext not provided');
+  }
+  const { showError } = errorContext;
 
   const handleFocus = () => {
     dispatch(setFocus(true));
@@ -51,13 +56,15 @@ const ResizableTextArea: React.FC<ResizableTextAreaProps> = ({ setIsCircle, quer
 
   const handleSearch = () => {
     setIsCircle(true);
-    setShowEdges(false)
+    setShowEdges(false);
     refetch({ query }).then((response) => {
       if (response.data.search) {
         dispatch(setResults(response.data.search));
       } else {
         dispatch(setResults([]));
       }
+    }).catch((err: ApolloError) => {
+      showError(err.message || 'An error occurred during the search');
     });
   };
 
@@ -78,8 +85,10 @@ const ResizableTextArea: React.FC<ResizableTextAreaProps> = ({ setIsCircle, quer
         onFocus={handleFocus}
         onBlur={handleBlur}
         onKeyDown={handleKeyDown}
+        disabled={loading}
       />
       <FontAwesomeIcon icon={faSearch} className={styles.searchIcon} onClick={handleSearch} size="lg" />
+      {loading && <Loader />}
     </div>
   );
 };
