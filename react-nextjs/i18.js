c0bf1f1d7f06b4411f438a4a9acbc5060b8c0e01
teteteo619 <120061560+aratetete0619@users.noreply.github.com>
2023-08-24 04:04:02 +0900

    :fix: Fix i18js

--- react-nextjs/i18.js
+++ react-nextjs/i18.js
@@ -1,27 +1,24 @@
 import i18n from 'i18next';
+import Backend from 'i18next-fs-backend';
 import { initReactI18next } from 'react-i18next';
+import path from 'path';
 
 i18n
-  .use(initReactI18next) // バインドする
+  .use(Backend)
+  .use(initReactI18next)
   .init({
-    resources: {
-      en: {
-        translation: {
-          "key": "Hello World"
-        }
-      },
-      ja: {
-        translation: {
-          "key": "こんにちは、世界"
-        }
-      }
+    backend: {
+      loadPath: path.join(process.cwd(), 'locales/{{lng}}/{{ns}}.json'),
+      addPath: path.join(process.cwd(), 'locales/{{lng}}/{{ns}}.missing.json'),
     },
-    lng: "en", // デフォルトの言語
-    fallbackLng: "en", // リソースが見つからない場合のフォールバック
-
+    lng: "en-US",
+    fallbackLng: "en-US",
+    supportedLngs: ['en-US', 'ja-JP'],
+    ns: ['Background', 'Features', 'Footer', 'Participation', 'Recruitment', 'Registration', 'Sidebar', 'Summary1', 'Summary2', 'Summary3', 'common'],
+    defaultNS: 'common',
     interpolation: {
-      escapeValue: false
-    }
+      escapeValue: false,
+    },
   });
 
 export default i18n;
