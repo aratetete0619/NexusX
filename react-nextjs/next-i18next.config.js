13d5e7f54ad585c82aca2739b9a6925729ae4c5b
teteteo619 <120061560+aratetete0619@users.noreply.github.com>
2023-08-24 04:47:44 +0900

    :fix: Fix i18js

--- react-nextjs/next-i18next.config.js
+++ react-nextjs/next-i18next.config.js
@@ -1,9 +1,6 @@
-const path = require('path');
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  swcMinify: false,
+}
 
-module.exports = {
-  i18n: {
-    defaultLocale: 'en-US',
-    locales: ['en-US', 'ja-JP'],
-  },
-  localePath: path.resolve('./locales'),
-};
+module.exports = nextConfig;
