8969432730a5ecf6a6e90b2ee5bbc15740d976f4
teteteo619 <120061560+aratetete0619@users.noreply.github.com>
2023-08-24 04:24:15 +0900

    :fix: Fix i18js

--- react-nextjs/next-i18next.config.js
+++ react-nextjs/next-i18next.config.js
@@ -6,4 +6,9 @@ module.exports = {
     locales: ['en-US', 'ja-JP'],
   },
   localePath: path.resolve('./locales'),
+  use: ['i18next-http-middleware', 'i18next-fs-backend'],
+  backend: {
+    loadPath: path.join(__dirname, 'locales/{{lng}}/{{ns}}.json'),
+    addPath: path.join(__dirname, 'locales/{{lng}}/{{ns}}.missing.json'),
+  }
 };
